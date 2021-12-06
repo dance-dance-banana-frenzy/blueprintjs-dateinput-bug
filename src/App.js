f2b688a7f9784fef336c1b9518d54c9901f82c56
Lewis Moten <lewis.moten@deciphernow.com>
2021-12-06 14:50:17 -0500

    :sparkle: change formatted date up to today

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React , { useState } from 'react';
 import { DateInput } from '@blueprintjs/datetime';
+import isValid from 'date-fns/isValid';
+import isDate from 'date-fns/isDate';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
 
-const App =  () => (<DateInput />)
+const FORMAT = 'yyyy-MM-dd';
+
+const now = new Date();
+
+const App =  () => {
+  
+  const [value, setValue] = useState(now);
+  
+  const onChange = value => {
+    setValue(value);
+  }
+  
+  return (
+    <DateInput
+      value={value}
+      onChange={onChange}
+      maxDate={now}
+      placeholder={FORMAT}
+      parseDate={parseDate}
+      formatDate={formatDate}
+    />
+  );
+}
+
+const parseDate = date  => parse(date, FORMAT, now);
+
+const formatDate = date => {
+  if (!isDate(date) || !isValid(date)) return 'Invalid Date';
+  return format(date, FORMAT);
+}
 
 export default App;
