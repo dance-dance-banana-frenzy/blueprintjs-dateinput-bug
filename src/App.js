66cf97389db3b26be5a1ecefa12d71245d522853
Lewis Moten <lewis.moten@deciphernow.com>
2021-12-06 16:00:52 -0500

    :lipstick: add blueprintjs core/popover2 styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import isValid from 'date-fns/isValid';
 import isDate from 'date-fns/isDate';
 import format from 'date-fns/format';
 import parse from 'date-fns/parse';
+
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
 
 const FORMAT = 'yyyy-MM-dd';
